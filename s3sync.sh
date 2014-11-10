30ba1f5a5295eafd81c5a2486c2f5c4d5edfe21b
Stephen Butterfill <stephen.butterfill@gmail.com>
2014-11-10 21:39:35 +0000

    updated

--- s3sync.sh
+++ s3sync.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-s3cmd sync --delete-removed ~out/ s3://www.butterfill.com --add-header "Cache-Control: max-age=86400"
+#nb do not --delete-removed because talks under subdirectories!
+
+s3cmd sync  out/ s3://www.butterfill.com --add-header "Cache-Control: max-age=86400"
 
