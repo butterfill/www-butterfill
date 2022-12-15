6a21a3e88ea8c1bf92e2ee0faa6e4be80449c7e7
Stephen Butterfill <stephen@butterfill.com>
2022-12-15 17:35:34 +0000

    [no message]

--- s3sync.sh
+++ s3sync.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 
-docpad clean
-docpad generate --env static
+node_modules/docpad/bin/docpad clean
+node_modules/docpad/bin/docpad generate --env static
 #nb do not --delete-removed because talks under subdirectories!
 s3cmd sync  out/ s3://www.butterfill.com --add-header "Cache-Control: max-age=86400"
 
